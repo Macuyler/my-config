ce489f6425f6a7ed35da5611d6af4e88d32df2c2
Macuyler Dunn <macuyler@gmail.com>
2021-04-26 08:32:17 -0500

    Allow right prompt color customization

--- dotfiles/prompt.zsh
+++ dotfiles/prompt.zsh
@@ -1,11 +1,19 @@
+# Set Host Color
 if [[ "$1" != "" ]]; then
-	HOST_COLOR="$1"
+	PROMPT_HOST_COLOR="$1"
 else
-	HOST_COLOR='white'
+	PROMPT_HOST_COLOR='white'
 fi
+# Set Directory Color
+if [[ "$2" != "" ]]; then
+	PROMPT_DIR_COLOR="$2"
+else
+	PROMPT_DIR_COLOR='white'
+fi
+# Get the branch of the current dir
 git_prompt() {
 	gitmux -cfg ~/.gitmux.conf $pwd | format-branch
 }
 setopt prompt_subst
-export RPROMPT='%F{cyan}%1~$(git_prompt)%f '
-export PROMPT=" %F{$HOST_COLOR}%M%f %# "
+export RPROMPT='%F{'"$PROMPT_DIR_COLOR"'}%1~$(git_prompt)%f '
+export PROMPT=" %F{$PROMPT_HOST_COLOR}%M%f %# "
